a5cd94a35d2bfe53804431c1c1de68e33fc458da
LtShibby <wozniamj01@gmail.com>
2025-02-18 17:39:02 +0800

    adding dist

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true
   },
