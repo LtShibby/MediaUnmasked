a70cab869cde3b72b61dfe84610a751d2aafdeb2
LtShibby <wozniamj01@gmail.com>
2025-02-18 22:40:55 +0800

    update reqs

--- api/build.sh
+++ api/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-set -e  # Exit on error
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
-python -m pip install --no-cache-dir -r requirements.txt
+# Ensure transformers is installed
+python -m pip install transformers
+
 # Ensure NLTK downloads are stored in /tmp
 mkdir -p /tmp/nltk_data
 python3 -c "import nltk; nltk.data.path.append('/tmp/nltk_data'); nltk.download('punkt', download_dir='/tmp/nltk_data'); nltk.download('averaged_perceptron_tagger', download_dir='/tmp/nltk_data')"
